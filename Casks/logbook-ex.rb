ad9362c47ae8130831a5646058a96f7ef349499b
jumitaka <jum@juns-MBP.local>
2014-12-29 11:44:19 +0900

    航海日誌 拡張版 (x64) Add
    http://nekopanda.blog.jp/

---
+++ Casks/logbook-ex.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'logbook-ex' do
+  version '1.4.0'
+  sha256 :no_check
+
+  url "http://nekopandanet.sakura.ne.jp/logbook/release/#{version}/logbook.macosx-x64-ex.zip"
+  name 'logbooke-ex'
+  homepage 'http://nekopanda.blog.jp/'
+  license :mit
+
+  artifact 'macosx-x64-ex', :target => 'logbook-ex'
+end
