a28d0135b403998e3f7567b6819e1eec6145b18b
jumitaka <jum@juns-MBP.local>
2014-12-29 11:43:47 +0900

    航海日誌(x64) Add
    http://kancolle.sanaechan.net/

---
+++ Casks/logbook.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'logbook' do
+  version :latest
+  sha256 :no_check
+
+  url "http://kancolle.sanaechan.net/svn/logbook/trunk/dest/logbook.macosx-x64.zip"
+  name 'logbook'
+  homepage 'http://kancolle.sanaechan.net/'
+  license :mit
+
+  artifact 'macosx-x64', :target => 'logbook'
+end
