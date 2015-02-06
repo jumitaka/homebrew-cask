3b852c8a462f218d484dcfa432747202f04a8a0e
jumitaka <jum@juns-MBP.local>
2015-02-06 23:27:58 +0900

    fix

--- Casks/logbook-ex.rb
+++ Casks/logbook-ex.rb
@@ -3,7 +3,7 @@ cask :v1 => 'logbook-ex' do
   sha256 :no_check
 
   url "http://nekopandanet.sakura.ne.jp/logbook/release/1.5.0/logbook.macosx-x64-ex.zip"
-  name 'logbooke-ex'
+  name 'logbook-ex'
   homepage 'http://nekopanda.blog.jp/'
   license :mit
 
