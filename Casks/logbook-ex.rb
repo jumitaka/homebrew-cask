d1108d52e8616c4470a170c6b59a7a8b1a1af954
jumitaka <jum@juns-MBP.local>
2015-07-01 01:18:22 +0900

    ver up url change

--- Casks/logbook-ex.rb
+++ Casks/logbook-ex.rb
@@ -2,7 +2,7 @@ cask :v1 => 'logbook-ex' do
   version :latest
   sha256 :no_check
 
-  url "http://nekopandanet.sakura.ne.jp/logbook/release/1.8.x/logbook.macosx-x64-ex.zip"
+  url "https://github.com/nekopanda/logbook/releases/download/v1.9.1/logbook.macosx-x64-ex.zip"
   name 'logbook-ex'
   homepage 'http://nekopanda.blog.jp/'
   license :mit
