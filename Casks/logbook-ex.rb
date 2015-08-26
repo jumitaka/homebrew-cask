54ea62a225b6801ee820208dfd5ec80930ff7394
jumitaka <jum@juns-MBP.local>
2015-08-26 19:22:28 +0900

    Ver up

--- Casks/logbook-ex.rb
+++ Casks/logbook-ex.rb
@@ -2,7 +2,7 @@ cask :v1 => 'logbook-ex' do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/nekopanda/logbook/releases/download/v1.9.1/logbook.macosx-x64-ex.zip"
+  url "https://github.com/nekopanda/logbook/releases/download/v1.9.3/logbook.macosx-x64-ex.zip"
   name 'logbook-ex'
   homepage 'http://nekopanda.blog.jp/'
   license :mit
