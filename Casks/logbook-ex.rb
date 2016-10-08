f8f9ed59b51da8c3ca7fc857a7f667e78d4baa20
jumitaka <jun22sumi26@gmail.com>
2016-10-08 21:42:37 +0900

    fix.

--- Casks/logbook-ex.rb
+++ Casks/logbook-ex.rb
@@ -2,10 +2,10 @@ cask 'logbook-ex' do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/nekopanda/logbook/releases/download/v2.1.9/logbook.macosx-x64-ex.zip"
+  url "https://github.com/nekopanda/logbook/releases/download/v2.2.4/logbook.macosx-x64-ex.zip"
   name 'logbook-ex'
   homepage 'http://nekopanda.blog.jp/'
   license :mit
 
-  artifact 'macosx-x64-ex', :target => 'logbook-ex'
+  artifact 'macosx-x64-ex', :target: '#{appdir}/logbook-ex'
 end
