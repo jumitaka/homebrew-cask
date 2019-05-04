1a881d4a4bf5eb76a0c1d0aaef18ba722eba4716
Jum <jum@JumMBP.local>
2019-05-04 15:20:55 +0900

    Del

--- Casks/logbook-ex.rb
+++
@@ -1,9 +0,0 @@
-cask 'logbook-ex' do
-  version :latest
-  sha256 :no_check
-
-  url "https://github.com/nekopanda/logbook/releases/download/v2.2.4/logbook.macosx-x64-ex.zip"
-  name 'logbook-ex'
-  homepage 'http://nekopanda.blog.jp/'
-  license :mit
-end
